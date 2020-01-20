ec7bafa67a5233c44442017b718a8ecbfd34009f
gelbman20 <gelbman20@gmail.com>
2020-01-20 15:26:47 +0200

    sys: app component restyle

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './app.scss'
 import Header from "../header";
+import SearchPanel from "../search-panel";
 import Main from "../main/main";
 
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Main/>
+    <div className='app'>
+      <div className="container">
+        <Header/>
+        <SearchPanel/>
+        <Main/>
+      </div>
     </div>
   );
 }
