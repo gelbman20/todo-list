8a53ae145fd5cdf9afb7de3af3f96ffa7e8ff64e
gelbman20 <gelbman20@gmail.com>
2020-01-20 17:48:22 +0200

    sys: have added done and important dynamic state to each list item

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -2,29 +2,41 @@ import React from "react";
 import './list-item.scss';
 import {CheckIcon, ImportantIcon} from "../icons";
 
-function ListItem() {
-  return(
+function ListItem({data, toggleDone, toggleImportant}) {
+  const items = data.map(({id, label, done, important}) => {
+    let textClassName = 'todo-list-item-text';
+    let doneClassName = 'todo-list-item-icon todo-list-item-icon-check';
+    let importantClassName = 'todo-list-item-icon todo-list-item-icon-important';
+
+    if (done) {
+      textClassName += ' active';
+      doneClassName += ' active';
+    }
+
+    if (important) {
+      importantClassName += ' active';
+    }
+
+    function iconDoneClick() {
+      toggleDone(id);
+    }
+
+    function iconImportantClick() {
+      toggleImportant(id)
+    }
+
+    return (
+      <li key={id} className='todo-list-item'>
+        <CheckIcon clickCheckIcon={ iconDoneClick } className={doneClassName}/>
+        <span className={textClassName}>{label}</span>
+        <ImportantIcon clickImportantIcon={ iconImportantClick } className={importantClassName}/>
+      </li>
+    );
+  });
+
+  return (
     <React.Fragment>
-      <li className='todo-list-item'>
-        <CheckIcon className='todo-list-item-icon todo-list-item-icon-check' />
-        <span className='todo-list-item-text'>Drink Some coffee</span>
-        <ImportantIcon className='todo-list-item-icon todo-list-item-icon-important'/>
-      </li>
-      <li className='todo-list-item'>
-        <CheckIcon className='todo-list-item-icon todo-list-item-icon-check' />
-        <span className='todo-list-item-text'>Eat Burger</span>
-        <ImportantIcon className='todo-list-item-icon todo-list-item-icon-important'/>
-      </li>
-      <li className='todo-list-item'>
-        <CheckIcon className='todo-list-item-icon todo-list-item-icon-check' />
-        <span className='todo-list-item-text'>Order Pizza</span>
-        <ImportantIcon className='todo-list-item-icon todo-list-item-icon-important active'/>
-      </li>
-      <li className='todo-list-item'>
-        <CheckIcon className='todo-list-item-icon todo-list-item-icon-check active' />
-        <span className='todo-list-item-text active'>Create Good Todo App</span>
-        <ImportantIcon className='todo-list-item-icon todo-list-item-icon-important'/>
-      </li>
+      {items}
     </React.Fragment>
   );
 }
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,12 +1,65 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import './todo-list.scss';
 import ListItem from "../list-item";
 
+function itemIndex(arr, id) {
+  const currentItem = arr.filter((item) => {
+    if (item.id === id) {
+      return item;
+    }
+  });
+
+  return arr.indexOf(...currentItem);
+}
+
+function toggleProperty(arr, id, property) {
+  const index = itemIndex(arr, id);
+  const currentItem = arr[index];
+  currentItem[property] = !currentItem[property];
+  const arrLeft = arr.slice(0, index);
+  const arrRight = arr.slice(index + 1);
+
+  return [...arrLeft, currentItem, ...arrRight];
+}
+
 export default class TodoList extends Component {
+
+  data = [
+    {id: 0, label: 'Drink Some coffee', done: false, important: false},
+    {id: 1, label: 'Eat Burger', done: false, important: true},
+    {id: 2, label: 'Order Pizza', done: false, important: false},
+    {id: 3, label: 'Create Good Todo App', done: true, important: false},
+  ];
+
+  state = {
+    data: this.data
+  };
+
+  toggleDone = (id) => {
+    const result = toggleProperty(this.state.data, id, 'done');
+    this.setState(() => {
+      return {
+        data: result
+      }
+    });
+  };
+
+  toggleImportant = (id) => {
+    const result = toggleProperty(this.state.data, id, 'important');
+    this.setState(() => {
+      return {
+        data: result
+      }
+    });
+  };
+
   render() {
+
+    const {data} = this.state;
+
     return (
       <ul className='todo-list'>
-        <ListItem />
+        <ListItem data={data} toggleDone={ this.toggleDone } toggleImportant={this.toggleImportant}/>
       </ul>
     );
   }
