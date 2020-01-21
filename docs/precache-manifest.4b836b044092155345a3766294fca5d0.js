5f6490b0523e45df0d7c92219d9ac082db6a168c
gelbman20 <gelbman20@gmail.com>
2020-01-21 14:34:31 +0200

    add: docs folder

---
+++ docs/precache-manifest.4b836b044092155345a3766294fca5d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0074a43b458348d9f11e6bf8e0f07f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92f6e84cdd1444ddf7aa",
+    "url": "/static/css/main.afbc02c4.chunk.css"
+  },
+  {
+    "revision": "e65aacfdf564571406f6",
+    "url": "/static/js/2.8f32d16d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8f32d16d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92f6e84cdd1444ddf7aa",
+    "url": "/static/js/main.e6a8292f.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
